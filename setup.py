1a3d80746670554780c0cde58125fcce4ea33943
zaberaki <aki@zaber.com>
2018-01-09 23:41:35 -0800

    bump version

--- setup.py
+++ setup.py
@@ -3,10 +3,10 @@
 from setuptools import setup, find_packages
 
 setup(name='swampyer',
-      version='1.2',
+      version='1.3',
       description='Simple WAMP library with minimal external dependencies',
       url = 'https://github.com/zabertech/python-swampyer',
-      download_url = 'https://github.com/zabertech/python-swampyer/archive/1.2.tar.gz',
+      download_url = 'https://github.com/zabertech/python-swampyer/archive/1.3.tar.gz',
       author='Aki Mimoto',
       author_email='aki+swampyer@zaber.com',
       license='MIT',
