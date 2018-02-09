52b1ac8459a44bc8cbd56501247069d3d178de54
zaberaki <aki@zaber.com>
2018-02-08 20:19:00 -0800

    publish 1.8

--- setup.py
+++ setup.py
@@ -3,10 +3,10 @@
 from setuptools import setup, find_packages
 
 setup(name='swampyer',
-      version='1.7',
+      version='1.8',
       description='Simple WAMP library with minimal external dependencies',
       url = 'https://github.com/zabertech/python-swampyer',
-      download_url = 'https://github.com/zabertech/python-swampyer/archive/1.7.tar.gz',
+      download_url = 'https://github.com/zabertech/python-swampyer/archive/1.8.tar.gz',
       author='Aki Mimoto',
       author_email='aki+swampyer@zaber.com',
       license='MIT',
