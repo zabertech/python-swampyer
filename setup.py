73668014860dbee4cc294513f3e05eb668b0d5b3
zaberaki <aki@zaber.com>
2018-05-19 08:02:06 -0700

    bring up to 1.13

--- setup.py
+++ setup.py
@@ -3,10 +3,10 @@
 from setuptools import setup, find_packages
 
 setup(name='swampyer',
-      version='1.12',
+      version='1.13',
       description='Simple WAMP library with minimal external dependencies',
       url = 'https://github.com/zabertech/python-swampyer',
-      download_url = 'https://github.com/zabertech/python-swampyer/archive/1.12.tar.gz',
+      download_url = 'https://github.com/zabertech/python-swampyer/archive/1.13.tar.gz',
       author='Aki Mimoto',
       author_email='aki+swampyer@zaber.com',
       license='MIT',
